c9b26068ab28ee9b4701926e592f4bfd77ef2445
David Tout <david@zendient.com>
2026-01-25 13:18:05 +1100

    Replace logo; Adjust elemental weights

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,5 @@
-import logoImage from "../assets/zen-logo-brush-512.png";
+// import logoImage from "../assets/zen-logo-brush-512-orig.png";
+import logoImage from "../assets/zen-logo-brush-512-retint.png";
 
 interface LogoProps {
   src?: string;
@@ -17,11 +18,11 @@ export function Logo({ src = logoImage, alt = "Zendient Labs" }: LogoProps) {
         className="relative w-32 h-32 md:w-48 md:h-48 flex items-center justify-center"
         style={{
           maskImage: `
-            linear-gradient(to bottom, transparent 0%, black  5%, black 96%, transparent 100%),
+            linear-gradient(to bottom, transparent 0%, black  8%, black 95%, transparent 100%),
             linear-gradient(to right,  transparent 0%, black 10%, black 90%, transparent 100%)
           `,
           WebkitMaskImage: `
-            linear-gradient(to bottom, transparent 0%, black  5%, black 96%, transparent 100%),
+            linear-gradient(to bottom, transparent 0%, black  8%, black 95%, transparent 100%),
             linear-gradient(to right,  transparent 0%, black 10%, black 90%, transparent 100%)
           `,
           maskComposite: "intersect",
